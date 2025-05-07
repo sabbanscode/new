97e0996a61ddbaed179fbcc0d626a372bb20aa63
Shubham Sabban <156825177+sabbanscode@users.noreply.github.com>
2025-05-07 09:36:38 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,12 @@
+# Simple Python Program
+
+# Print a message
+print("Hello, World!")
+
+# Add two numbers
+a = 5
+b = 3
+sum = a + b
+
+# Print the result
+print("The sum of", a, "and", b, "is", sum)
